162ecb76f5d88f21e28cc98b870a27c9b1613073
Juanma <jmgalang07@iesalbarregas.es>
2024-10-02 10:49:03 +0200

    Jsp y ciclo de vida
    Añado un jsp al proyecto con una calculadora y hago el ciclo de vida de un servlet's.

---
+++ src/main/java/es/albarregas/controllers/CicloDeVida.java
@@ -0,0 +1,53 @@
+package es.albarregas.controllers;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet que demuestra el ciclo de vida de un servlet.
+ * 
+ * @author Juanma
+ */
+@WebServlet(name = "CicloDeVida", urlPatterns = {"/CicloDeVida"})
+public class CicloDeVida extends HttpServlet {
+
+    // Se ejecuta cuando el servlet es inicializado
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        System.out.println("Servlet inicializado (init).");
+    }
+
+    /**
+     * Maneja las solicitudes HTTP <code>GET</code>.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException si ocurre un error específico del servlet
+     * @throws IOException si ocurre un error de entrada/salida
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        System.out.println("Solicitud GET recibida (doGet).");
+
+        // Configura el tipo de contenido de la respuesta
+        response.setContentType("text/html;charset=UTF-8");
+
+        // Escribe la respuesta al cliente
+        response.getWriter().println("<html><body>");
+        response.getWriter().println("<h1>Este es el ciclo de vida de un servlet.</h1>");
+        response.getWriter().println("</body></html>");
+    }
+
+    // Se ejecuta cuando el servlet está a punto de ser destruido
+    @Override
+    public void destroy() {
+        System.out.println("Servlet destruido (destroy).");
+        super.destroy();
+    }
+}
